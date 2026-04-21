4ad55e44642226a7df6830fde45228d1f12df4ef
Pranshu Srivastava <srivastavapranshu2@gmail.com>
2026-04-21 17:21:45 +0530

    enqueue all reminders with supported send-followup-reminder job name

---
+++ src/scheduler/followupemail.scheduler.js
@@ -0,0 +1,97 @@
+import { followupQueue } from "../queues/followup.queue.js";
+class FollowUpEmailScheduler {
+  static async sendFollowupEmailJob(
+    followUpId,
+    hrEmail,
+    role,
+    company,
+    appliedDate,
+    hrName,
+    userName,
+    userEmail,
+  ) {
+    try {
+      if (!hrEmail) {
+        throw new Error("Invalid user object passed to sendFollowupEmailJob");
+      }
+
+      await followupQueue.add(
+        "send-followup-reminder",
+        {
+          followUpId,
+          to: hrEmail,
+          role,
+          company,
+          appliedDate,
+          hrName,
+          userName,
+          userEmail,
+        },
+        {
+          delay: 5000,
+          attempts: 3,
+          backoff: {
+            type: "exponential",
+            delay: 5000,
+          },
+          removeOnComplete: true,
+          removeOnFail: false,
+        },
+      );
+
+      await followupQueue.add(
+        "send-followup-reminder",
+        {
+          followUpId,
+          to: hrEmail,
+          role,
+          company,
+          appliedDate,
+          hrName,
+          userName,
+          userEmail,
+        },
+        {
+          delay: 7 * 24 * 60 * 60 * 1000,
+          attempts: 3,
+          backoff: {
+            type: "exponential",
+            delay: 5000,
+          },
+          removeOnComplete: true,
+          removeOnFail: false,
+        },
+      );
+
+      await followupQueue.add(
+        "send-followup-reminder",
+        {
+          followUpId,
+          to: hrEmail,
+          role,
+          company,
+          appliedDate,
+          hrName,
+          userName,
+          userEmail,
+        },
+        {
+          delay: 14 * 24 * 60 * 60 * 1000,
+          attempts: 3,
+          backoff: {
+            type: "exponential",
+            delay: 5000,
+          },
+          removeOnComplete: true,
+          removeOnFail: false,
+        },
+      );
+    } catch (error) {
+      const err = new Error("Failed to enqueue followup job");
+      err.cause = error;
+      throw err;
+    }
+  }
+}
+
+export default FollowUpEmailScheduler;
