1bddc3ea3882dbe771e2159f8fcbdc217aa04d4a
Pranshu Srivastava <srivastavapranshu2@gmail.com>
2026-04-30 19:36:01 +0530

    feat(application.routes): add validated update application route

--- src/modules/application/application.routes.js
+++ src/modules/application/application.routes.js
@@ -3,7 +3,10 @@ import { Router } from "express";
 import { ApplicationController } from "./application.controller.js";
 import authenticate from "../../middlewares/auth.middleware.js";
 import validateRequest from "../../middlewares/validateRequest.js";
-import { createApplicationSchema } from "../../validators/application.schema.js";
+import {
+  createApplicationSchema,
+  updateApplicationSchema,
+} from "../../validators/application.schema.js";
 const router = Router();
 
 router.post(
@@ -27,7 +30,12 @@ router.get(
 );
 
 router.get("/:id", authenticate, ApplicationController.getApplicationById);
-router.patch("/:id", authenticate, ApplicationController.updateApplication);
+router.patch(
+  "/update/:id",
+  authenticate,
+  validateRequest(updateApplicationSchema),
+  ApplicationController.updateApplication,
+);
 router.delete("/:id", authenticate, ApplicationController.deleteApplication);
 
 router.get("/:id/followups", authenticate, ApplicationController.getFollowUps);
