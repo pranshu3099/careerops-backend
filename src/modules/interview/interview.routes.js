2be76ce391162730fc153a1a0b739bfb706fa2a2
Pranshu Srivastava <srivastavapranshu2@gmail.com>
2026-05-06 10:20:19 +0530

    feat(src/modules/interview/interview.routes.js): add interview API routes.

---
+++ src/modules/interview/interview.routes.js
@@ -0,0 +1,43 @@
+import { Router } from "express";
+
+import authenticate from "../../middlewares/auth.middleware.js";
+import validateRequest from "../../middlewares/validateRequest.js";
+import {
+  createInterviewSchema,
+  updateInterviewResultSchema,
+  updateInterviewSchema,
+} from "../../validators/interview.schema.js";
+import { InterviewController } from "./interview.controller.js";
+
+const router = Router();
+
+router.get("/", authenticate, InterviewController.getAllInterviews);
+
+router.post(
+  "/",
+  authenticate,
+  validateRequest(createInterviewSchema),
+  InterviewController.createInterview,
+);
+
+router.get(
+  "/application/:applicationId",
+  authenticate,
+  InterviewController.getApplicationInterviews,
+);
+
+router.patch(
+  "/:id",
+  authenticate,
+  validateRequest(updateInterviewSchema),
+  InterviewController.updateInterview,
+);
+
+router.patch(
+  "/:id/result",
+  authenticate,
+  validateRequest(updateInterviewResultSchema),
+  InterviewController.updateInterviewResult,
+);
+
+export default router;
