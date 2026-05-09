ca09904115c9c1d79b67ced2a36642c171a6089b
Pranshu Srivastava <srivastavapranshu2@gmail.com>
2026-05-09 13:12:06 +0530

    feat: add analytics controller

---
+++ src/modules/analytics/analytics.controller.js
@@ -0,0 +1,76 @@
+import { HTTP_STATUS } from "../../constants/httpStatus.js";
+import { AnalyticsService } from "./analytics.service.js";
+
+const getAuthUserId = (req) => req?.user?.userId || req?.user?.id;
+
+export class AnalyticsController {
+  static async getOverview(req, res) {
+    try {
+      const data = await AnalyticsService.getOverview(getAuthUserId(req));
+      return res.status(HTTP_STATUS.OK).json(data);
+    } catch (e) {
+      return res
+        .status(HTTP_STATUS.INTERNAL_SERVER_ERROR)
+        .json({ message: e.message });
+    }
+  }
+
+  static async getFunnel(req, res) {
+    try {
+      const data = await AnalyticsService.getFunnel(getAuthUserId(req));
+      return res.status(HTTP_STATUS.OK).json(data);
+    } catch (e) {
+      return res
+        .status(HTTP_STATUS.INTERNAL_SERVER_ERROR)
+        .json({ message: e.message });
+    }
+  }
+
+  static async getTimeline(req, res) {
+    try {
+      const data = await AnalyticsService.getTimeline(getAuthUserId(req), {
+        range: req.query.range,
+        bucket: req.query.bucket,
+      });
+      return res.status(HTTP_STATUS.OK).json(data);
+    } catch (e) {
+      return res
+        .status(HTTP_STATUS.INTERNAL_SERVER_ERROR)
+        .json({ message: e.message });
+    }
+  }
+
+  static async getSources(req, res) {
+    try {
+      const data = await AnalyticsService.getSources(getAuthUserId(req));
+      return res.status(HTTP_STATUS.OK).json(data);
+    } catch (e) {
+      return res
+        .status(HTTP_STATUS.INTERNAL_SERVER_ERROR)
+        .json({ message: e.message });
+    }
+  }
+
+  static async getInterviews(req, res) {
+    try {
+      const data = await AnalyticsService.getInterviews(getAuthUserId(req));
+      return res.status(HTTP_STATUS.OK).json(data);
+    } catch (e) {
+      return res
+        .status(HTTP_STATUS.INTERNAL_SERVER_ERROR)
+        .json({ message: e.message });
+    }
+  }
+
+  static async getTimeMetrics(req, res) {
+    try {
+      const data = await AnalyticsService.getTimeMetrics(getAuthUserId(req));
+      return res.status(HTTP_STATUS.OK).json(data);
+    } catch (e) {
+      return res
+        .status(HTTP_STATUS.INTERNAL_SERVER_ERROR)
+        .json({ message: e.message });
+    }
+  }
+}
+
