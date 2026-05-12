6651e7b1db54c5755a2b5d20400ade19b743bbe2
Pranshu Srivastava <srivastavapranshu2@gmail.com>
2026-05-12 18:49:59 +0530

    feat(app): add helmet json limit and origin checks

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import helmet from "helmet";
 import passport from "./config/passport.js";
 import authRoutes from "../src/modules/auth/auth.routes.js";
 import applicationRoutes from "../src/modules/application/application.routes.js";
@@ -11,16 +12,19 @@ import interviewRoutes from "../src/modules/interview/interview.routes.js";
 import settingsRoutes from "../src/modules/settings/settings.routes.js";
 import analyticsRoutes from "../src/modules/analytics/analytics.routes.js";
 import notificationRoutes from "../src/modules/notification/notification.routes.js";
+import { originCheck } from "./middlewares/csrf.middleware.js";
 
 const app = express();
+app.use(helmet());
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
-app.use(express.json())
+app.use(express.json({ limit: "100kb" }));
 app.use(cookieParser());
+app.use(originCheck);
 app.use(passport.initialize());
 
 app.use("/auth", authRoutes);
