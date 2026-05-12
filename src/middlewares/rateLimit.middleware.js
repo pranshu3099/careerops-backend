d0c1ed6b7ee9c07c2e13fd1e5740a6002eafd724
Pranshu Srivastava <srivastavapranshu2@gmail.com>
2026-05-12 18:51:58 +0530

    feat(rate-limit): return shared status and message constants

--- src/middlewares/rateLimit.middleware.js
+++ src/middlewares/rateLimit.middleware.js
@@ -1,20 +1,23 @@
-import { slidingWindowRateLimiter } from "../utils/rateLimiter";
-const rateLimit = (limit, windowSizeInSeconds, keyGenerator) => async (req, res, next) => {
+import { slidingWindowRateLimiter } from "../utils/rateLimiter.js";
+import { HTTP_STATUS } from "../constants/httpStatus.js";
+import { COMMON_MESSAGES } from "../constants/messages.js";
+
+const rateLimit =
+  (limit, windowSizeInSeconds, keyGenerator) => async (req, res, next) => {
     const key = keyGenerator(req);
     const isAllowed = await slidingWindowRateLimiter({
-        key,
-        limit,
-        windowSizeInSeconds
+      key,
+      limit,
+      windowSizeInSeconds,
     });
+
     if (!isAllowed) {
-        return res.status(429).json({ error: "Too many requests" });
+      return res.status(HTTP_STATUS.TOO_MANY_REQUESTS).json({
+        error: COMMON_MESSAGES.TOO_MANY_REQUESTS,
+      });
     }
+
     next();
-}
+  };
+
 export default rateLimit;
-
-
-
-
-
-
